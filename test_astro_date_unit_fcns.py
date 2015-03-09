f68358792cb346c6ebe4b9c66b35f1825cb2badd
thewellington00 <thewellington00@users.noreply.github.com>
2015-03-08 17:47:51 -0700

    Updated calendar_day() test

--- test_astro_date_unit_fcns.py
+++ test_astro_date_unit_fcns.py
@@ -2,18 +2,15 @@
 
 from astro_date_unit_fcns import *
 import unittest
+import datetime
 
 class TestAstroDateUnitFcns(unittest.TestCase):
     def test_julian_day_default(self):
         self.assertEquals(2436116.30625, julian_day())
 
     def test_calendar_day_default(self):
-        actual_date = calendar_date()
-        self.assertEquals(1957, actual_date.year)
-        self.assertEquals(10, actual_date.month)
-        self.assertEquals(4, actual_date.day)
-        self.assertEquals(19, actual_date.hour)
-        self.assertEquals(21, actual_date.minute)
+        dt_answer = datetime.datetime(1957, 10, 4, 19, 21)
+        self.assertEquals(dt_answer, calendar_date())
 
 if __name__ == '__main__':
     unittest.main()
