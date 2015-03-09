f3b849860894fb0996d1845be149093c20fd99b7
thewellington00 <thewellington00@users.noreply.github.com>
2015-03-08 23:30:46 -0700

    was able to confirm that star_rise_set is now working

--- star_calc_rise_set.py
+++ star_calc_rise_set.py
@@ -18,13 +18,15 @@ def star_rise_set(dt_input=default_dt, rt_asc=rt_asc_arctarus, dec=dec_arctarus,
     For a specific star return the rise, transit and set times
     All inputes, except the datetime object, are assumed to be in degrees
 
+    The outputs are in UT time! Not sidereal time.
+
     These calculations were taken from Chapter 15 of Astronomy Algorithms
     """
     # an input needed for the math below
     h0_star = -0.5667
     # calculate sidereal time in greenwich at 0 UT
     day_only = datetime.datetime.combine(dt_input.date(), datetime.time(0))
-    sidereal_time = sidereal_time_greenwich(dt_input)
+    sidereal_time = sidereal_time_greenwich(day_only)
     # convert inputs into radians
     conv_rad = np.pi/180
     h0_star_rad = h0_star * conv_rad
@@ -47,33 +49,36 @@ def star_rise_set(dt_input=default_dt, rt_asc=rt_asc_arctarus, dec=dec_arctarus,
     if transit_deg > 360 or rise_deg > 360 or set_deg > 360:
         print 'Error: something is happening outside of this day'
 
-    # this time is the sidereal time at Greenwich
-    transit_time = decdeg2time(transit_deg)
-    rise_time = decdeg2time(rise_deg)
-    set_time = decdeg2time(set_deg)
-
     return transit_deg, rise_deg, set_deg
 
 def display_arctarus_example():
-    # display some example results
+    # display some example results, this is not checking out:
+    # the right ascension of the transit time does match now
+    # you can verify by inputing the "transit_time" into this webpage:
+    # http://www.csgnetwork.com/siderealjuliantimecalc.html
+    # the LST result should be equal to the right ascension
 
     # first pull in the default results, these are in degrees in Greenwich timezone
-    transit, rise, setting = star_rise_set()
+    transit, rise, setting = star_rise_set(datetime.datetime.utcnow())
     # convert them to local time
-    transit_local = decdeg2time(sidereal_time_local(transit))
-    rise_local = decdeg2time(sidereal_time_local(rise))
-    set_local = decdeg2time(sidereal_time_local(setting))
+    transit_time = decdeg2time(transit)
+    rise_time = decdeg2time(rise)
+    set_time = decdeg2time(setting)
+
+    # need to convert the UT time to local time
+    # should use the pytz package for this instead
+    transit_local = transit_time + datetime.timedelta(hours=-7)
+    rise_local = rise_time + datetime.timedelta(hours=-7)
+    set_local = set_time + datetime.timedelta(hours=-7)
 
     # print out some results
-    # why doesn't the local transit time equal the right ascension??
-    print 'Here are the results in Local Sidereal Time, these are wrong :('
-    print 'Transit time: ' + transit_local.isoformat()
-    print 'Rise time: ' + rise_local.isoformat()
-    print 'Set time: ' + set_local.isoformat()
-
+    print 'Here are the results for Arctarus in SF local time today:'
+    print 'Rise time: ' + rise_local.time().isoformat()
+    print 'Transit time: ' + transit_local.time().isoformat()    
+    print 'Set time: ' + set_local.time().isoformat()
 
 def main():
-    pass
+    display_arctarus_example()
 
 if __name__ == '__main__':
     main()
